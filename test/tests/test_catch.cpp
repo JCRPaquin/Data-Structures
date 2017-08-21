7cfd7e01756117c9d38519a426a854b7efcd704f
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2017-08-21 18:22:12 -0400

    Added test framework test

---
+++ test/tests/test_catch.cpp
@@ -0,0 +1,11 @@
+/**
+ * Created by Jean-Claude Paquin on 8/21/17.
+ *
+ * A simple test of the test framework.
+ **/
+
+#include "catch.hpp"
+
+TEST_CASE("Make sure Catch is working as expected") {
+    REQUIRE(true);
+}
