4d51714dd113e00e3d98957a3403fee361006d1c
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2017-08-22 18:23:57 -0400

    Add more tests for rope_node

--- test/tests/test_str_rope.cpp
+++ test/tests/test_str_rope.cpp
@@ -18,17 +18,35 @@ TEST_CASE("Empty rope_node", "[rope_node]") {
     SECTION("Empty node to string") {
         REQUIRE(*node->to_string().get() == "");
     }
+
+    SECTION("Empty node is not a leaf") {
+        REQUIRE(!node->is_leaf);
+    }
 }
 
 TEST_CASE("String rope_node", "[rope_node]") {
     std::unique_ptr<rope_node> node = std::make_unique<rope_node>(std::string("wow!"));
+
+    SECTION("String node is a leaf") {
+        REQUIRE(node->is_leaf);
+    }
 }
 
 TEST_CASE("Copy constructor for rope_node", "[rope_node]") {
-    std::unique_ptr<rope_node> node = std::make_unique<rope_node>(std::string("that's a low price!"));
+    SECTION("Copied node is a leaf node") {
+        std::unique_ptr<rope_node> node = std::make_unique<rope_node>(std::string("that's a low price!"));
 
-    std::unique_ptr<rope_node> dupl = std::make_unique<rope_node>(*node.get());
+        std::unique_ptr<rope_node> dupl = std::make_unique<rope_node>(*node.get());
 
-    REQUIRE(*dupl->to_string().get() == *node->to_string().get());
+        REQUIRE(*dupl->to_string().get() == *node->to_string().get());
+        REQUIRE(dupl->is_leaf);
+    }
+
+    SECTION("Copied node is not a leaf node") {
+        rope_node node;
+        rope_node dupl(node);
+
+        REQUIRE(!dupl.is_leaf);
+    }
 }
 
