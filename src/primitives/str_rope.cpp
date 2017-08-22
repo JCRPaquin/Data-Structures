865a7251e951106bd3c0b37e5a8a95daba89ca74
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2017-08-21 20:42:47 -0400

    Add rope_node class implementation

--- src/primitives/str_rope.cpp
+++ src/primitives/str_rope.cpp
@@ -6,3 +6,40 @@
  **/
 
 #include "str_rope.h"
+
+struct rope_node {
+    rope_node() : len(0), left(nullptr), right(nullptr) {}
+
+    rope_node(rope_node& other) : isLeaf(other.isLeaf) {
+        if(isLeaf) {
+            str = std::make_unique(other.str);
+        } else {
+            len = other.len;
+
+            if(other.left->isLeaf) {
+                left = other.left;
+            } else {
+                left = std::make_shared(rope_node(*other.left.get()));
+            }
+
+            if(other.right->isLeaf) {
+                right = other.right;
+            } else {
+                right = std::make_shared(rope_node(*other.right.get()));
+            }
+        }
+    }
+
+    rope_node(const std::string& str) : isLeaf(true) {
+        this->str = std::make_unique(str);
+    }
+
+    bool isLeaf = false;
+    union {
+        struct {
+            size_t len;
+            std::shared_ptr<rope_node> left, right;
+        };
+        std::unique_ptr<std::string> str;
+    };
+};
