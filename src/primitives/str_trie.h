1408ef6972c4c5dd89aa882f759c8ff8c8ee29fc
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2017-08-29 23:18:31 -0400

    Modified str_trie header comment

--- src/primitives/str_trie.h
+++ src/primitives/str_trie.h
@@ -1,6 +1,12 @@
-//
-// Created by Jean-Claude Paquin on 8/21/17.
-//
+/**
+ * Tries aren't actually a way to store strings. I just decided to leave them
+ *   here because they're very heavily related to strings.
+ *
+ * Tries are essentially a form of map from strings to other types of values.
+ *   They may outperform a hash map in certain scenarios.
+ *
+ * @author Jean-Claude Paquin
+ **/
 
 #ifndef DATA_STRUCTURES_STR_TRIE_H
 #define DATA_STRUCTURES_STR_TRIE_H
